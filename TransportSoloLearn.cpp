99831487f64cf6a19df69759fe5eae9eb266c4fc
Christian G <chrisgobin94@gmail.com>
2021-05-04 14:46:53 -0400

    Update TransportSoloLearn.cpp

--- TransportSoloLearn.cpp
+++ TransportSoloLearn.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main(){
    int total_people_waiting;
 
-   std::cin >> total_people_waiting;
+   cin >> total_people_waiting;
 
    if((total_people_waiting <= 50) && (total_people_waiting > 0)){
-      std::cout << total_people_waiting << endl;
+      cout << total_people_waiting << endl;
    } else {
       while(total_people_waiting > 50){
          total_people_waiting -= 50;
