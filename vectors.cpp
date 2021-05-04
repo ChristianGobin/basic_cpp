d709dbd9fd943924c170d9da92334be00327841f
Christian G <chrisgobin94@gmail.com>
2021-05-03 21:50:22 -0400

    Vector Practice File
    File demonstrates my understanding of vectors and familiarity with them.

---
+++ vectors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Declaring and Initializing Vectors
+//Accessing and Modifying Vectors
+
+int main(){
+	vector <int> age_of_students {1,2,3,4,5};
+	cout << age_of_students[4];
+	
+	return 0;
+}
